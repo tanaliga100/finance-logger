d8aad9b2612cfd877e0cb5b630acddd778545249
tanaliga100 <jordantanaliga@gmail.com>
2022-10-23 01:40:27 +0800

    exposed-dist

---
+++ dist/classes/invoice.js
@@ -0,0 +1,7 @@
+export class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+}
---
+++ dist/classes/payment.js
@@ -0,0 +1,7 @@
+export class Payment {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+}
